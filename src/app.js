37036447ea6d5bd80552b65134d3a96def86346f
vancerweston <vweston@student.neumont.edu>
2020-04-25 18:07:02 -0600

    built index.ejs, added paths, created server local:3000

--- src/app.js
+++ src/app.js
@@ -4,3 +4,14 @@ const express = require('express');
 
 const app = express();
 
+app.set('views', path.join(__dirname, '/views'));
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', function(req, res) {
+    res.render('index', { title: 'Index' });
+});
+
+app.listen(3000, () => console.log("PS Project Running on port 3000!"));
