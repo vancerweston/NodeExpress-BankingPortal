cc2c2edda2b34e591efb71fdec66934a9fcaa897
vancerweston <vweston@student.neumont.edu>
2020-04-25 18:31:27 -0600

    created prifle.ejs, created accounts, created path for profile.ejs

--- src/app.js
+++ src/app.js
@@ -10,8 +10,41 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+const accountData = fs.readFileSync('src/json/accounts.json', 'UTF8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json', 'UTF8');
+const users = JSON.parse(userData);
+
+app.get('/savings', function(req, res) {
+    res.render('account', {
+        account: accounts.savings
+    });
+});
+
+app.get('/checking', function(req, res) {
+    res.render('account', {
+        account: accounts.checking
+    });
+});
+
+app.get('/credit', function(req, res) {
+    res.render('account', {
+        account: accounts.credit
+    });
+});
+
+app.get('/profile', function(req,res) {
+    res.render('profile', {
+        user: users[0]
+    });
+});
+
 app.get('/', function(req, res) {
-    res.render('index', { title: 'Index' });
+    res.render('index', { 
+        title: 'Account Summary', 
+        accounts 
+    });
 });
 
 app.listen(3000, () => console.log("PS Project Running on port 3000!"));
